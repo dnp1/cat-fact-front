25ee0264476667aea7aee359a570fd9c28907cb4
Danilo Pereira <dnp1@users.noreply.github.com>
2019-01-12 18:56:18 -0200

    botao de pegar cat fact

--- src/components/FactView.js
+++ src/components/FactView.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios' 
+
 
 class FactView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {fact: null};
+        this.getFact()
+        // This binding is necessary to make `this` work in the callback
+        this.getFactEvent = this.getFactEvent.bind(this);
+    }
+    async getFact() {
+        const response = await axios.get('https://cat-fact.herokuapp.com/facts/random?animal=cat', { crossdomain: false })
+        this.setState({fact: response.data}) 
+    }
+
+    async getFactEvent(e){
+        const response = await axios.get('https://cat-fact.herokuapp.com/facts/random?animal=cat', { crossdomain: false })
+        this.setState({fact: response.data}) 
+    }
+
   render() {
     return (
-      <div>
-        Meu fato
+        <div>
+            <div>
+        { this.state.fact !== null ? 
+            (<p>Meu Fato: {this.state.fact.text}</p>)
+            : null}
+
       </div>
+      <div>
+          <button onClick={this.getFactEvent}>Get a new cat FACT!!!
+      </button>
+      </div>
+      </div>
+      
     );
   }
+
+  
 }
 
 export default FactView;
